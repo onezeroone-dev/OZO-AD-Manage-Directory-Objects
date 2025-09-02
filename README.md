f758794fd97322298f9a002ba842f341866369ef
Andy Lievertz <alievertz@onezeroone.dev>
2025-09-01 18:38:53 -0600

    save

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # OZO AD Manage Directory Objects Installation and Usage
 ## Description
-This script creates Active Directory contacts, computers, groups, and users based on a JSON configuration file.
+This script creates Active Directory contacts, computers, groups, [empty\] group policies, organizational units, and users based on a JSON configuration file.
 
 ## Prerequisites
-This script requires the _ActiveDirectory_, _ImportExcel_, _OZO_, _OZOAD_, _OZOFiles_,and _OZOLogger_ PowerShell modules. The ActiveDirectory, DFSN, DFSR, and GroupPolicy modules are included with the [Remote Server Administration Tools](https://learn.microsoft.com/en-us/troubleshoot/windows-server/system-management-components/remote-server-administration-tools) installation. The remaining modules are published to [PowerShell Gallery](https://learn.microsoft.com/en-us/powershell/scripting/gallery/overview?view=powershell-5.1). Ensure your system is configured for this repository then execute the following in an _Administrator_ PowerShell:
+This script requires the _ActiveDirectory_, _GroupPolicy_, _ImportExcel_, _OZO_, _OZOAD_, _OZOFiles_,and _OZOLogger_ PowerShell modules. The ActiveDirectory and GroupPolicy modules are included with the [Remote Server Administration Tools](https://learn.microsoft.com/en-us/troubleshoot/windows-server/system-management-components/remote-server-administration-tools) installation. The remaining modules are published to [PowerShell Gallery](https://learn.microsoft.com/en-us/powershell/scripting/gallery/overview?view=powershell-5.1). Ensure your system is configured for this repository then execute the following in an _Administrator_ PowerShell:
 
 ```powershell
-Install-Module ImportExcel,OZO,OZOAD,OZOFiles,OZOLogger
+Install-Module ImportExcel,OZO,OZOLogger
 ```
 
 ## Installation
@@ -27,6 +27,48 @@ ozo-ad-manage-directory-objects
 |---------|-----------|
 |`Configuration`|Path to the JSON configuration file. Defaults to `ozo-ad-manage-directory-objects.json` in the same directory as the script.|
 |`OutDir`|Directory for the Excel report. Defaults to the current directory.|
+
+## JSON Configuration Definition
+This script leverages the [One Zero One Unified JSON Schema](https://onezeroone.dev/ozo-unified-json-schema/). The elements of the schema used by this script are as follows. Please also see [ozo-ad-manage-directory-objects-EXAMPLE.json](https://github.com/onezeroone-dev/OZO-AD-Manage-Directory-Objects/blob/main/ozo-ad-manage-directory-objects-EXAMPLE.json).
+
+```json
+{
+    "ADContacts":[
+        {
+            "FirstName":"",
+            "LastName":"",
+            "EmailAddress":"",
+            "PhoneNumber":""
+        }
+    ],
+    "ADComputers":[
+        {
+            "ComputerName":"",
+            "Path":""
+        }
+    ],
+    "ADGroups":[
+        {
+            "Name":"",
+            "Path":"",
+            "Scope":0,
+            "Members":[]
+        }
+    ],
+    "ADUsers":[
+        {
+            "Surname":"",
+            "GivenName":"",
+            "Division":"",
+            "Title":"",
+            "EmployeeID":0,
+            "SamAccountName":"",
+            "Password":"",
+            "Path":""
+        }
+    ]
+}
+```
 
 ## Notes
 Run this script with a user that has rights to create comuters, contacts, groups, and users in the Active Directory domain.
--- ozo-ad-manage-directory-objects.ps1
+++ ozo-ad-manage-directory-objects.ps1
@@ -1,55 +1,59 @@
-#Requires -Modules ImportExcel,@{ModuleName="OZO";ModuleVersion="1.5.1"},OZOLogger -Version 5.1
+#Requires -Modules ImportExcel,OZO,OZOLogger -Version 5.1
 
 <#PSScriptInfo
     .VERSION 0.0.1
-    .GUID *** Generate a new GUID with New-GUID ***
+    .GUID 87eaa283-5e8b-4265-a025-ce1f5791491b
     .AUTHOR Andy Lievertz <alievertz@onezeroone.dev>
     .COMPANYNAME One Zero One
     .COPYRIGHT This script is released under the terms of the GNU General Public License ("GPL") version 2.0.
     .TAGS 
-    .LICENSEURI https://github.com/onezeroone-dev/OZO-PowerShell-Script-Template/blob/main/LICENSE
-    .PROJECTURI https://github.com/onezeroone-dev/OZO-PowerShell-Script-Template
+    .LICENSEURI https://github.com/onezeroone-dev/OZO-AD-Manage-Directory-Objects/blob/main/LICENSE
+    .PROJECTURI https://github.com/onezeroone-dev/OZO-AD-Manage-Directory-Objects
     .ICONURI 
-    .EXTERNALMODULEDEPENDENCIES ImportExcel
+    .EXTERNALMODULEDEPENDENCIES
     .REQUIREDSCRIPTS 
     .EXTERNALSCRIPTDEPENDENCIES 
-    .RELEASENOTES https://github.com/onezeroone-dev/OZO-PowerShell-Script-Template/blob/main/CHANGELOG.md
+    .RELEASENOTES https://github.com/onezeroone-dev/OZO-AD-Manage-Directory-Objects/blob/main/CHANGELOG.md
 #>
 
 <# 
     .SYNOPSIS
     See description.
     .DESCRIPTION 
-    Reads a JSON configuration file and produces an Excel report.
+    Creates Active Directory users and groups based on a JSON configuration file.
     .PARAMETER Configuration
-    Path to the JSON configuration file. Defaults to "ozo-powershell-script-template.json" in the same directory as the script.
+    Path to the JSON configuration file. Defaults to "ozo-ad-manage-directory-objects.json" in the same directory as the script.
     .PARAMETER OutDir
     Directory for the Excel report. Defaults to the current directory.
     .LINK
-    https://github.com/onezeroone-dev/OZO-PowerShell-Script-Template/blob/main/README.md
+    https://github.com/onezeroone-dev/OZO-AD-Manage-Directory-Objects/blob/main/README.md
 #>
 
 # PARAMETERS
-[CmdletBinding(SupportsShouldProcess = $true)]
-param (
-    [Parameter(Mandatory=$false,HelpMessage="Path to the JSON configuration file")][String]$Configuration = (Join-Path -Path $PSScriptRoot -ChildPath "ozo-powershell-script-template.json"),
+[CmdletBinding(SupportsShouldProcess = $true)] Param (
+    [Parameter(Mandatory=$false,HelpMessage="Path to the JSON configuration file")][String]$Configuration = (Join-Path -Path $PSScriptRoot -ChildPath "ozo-ad-manage-directory-objects.json"),
     [Parameter(Mandatory=$false,HelpMessage="Path for the Excel report")][String]$OutDir = (Get-Location)
 )
 
 # CLASSES
 Class OZOMain {
     # PROPERTIES: Booleans, Strings
-    [Boolean] $Success   = $true
+    [Boolean] $Success   = $false
+    [String]  $adDomain  = $null
     [String]  $jsonPath  = $null
     [String]  $outDir    = $null
     [String]  $excelPath = $null
     # PROPERTIES: PSCustomObjects
     [PSCustomObject] $Json   = $null
     [PSCustomObject] $Logger = $null
-    # PROPERTIES: Lists
-    [System.Collections.Generic.List[PSCustomObject]] $Items = @()
-    # METHODS
-    # Constructor method
+    # PROPERTIES: PSCustomObject Lists
+    [System.Collections.Generic.List[PSCustomObject]] $adOUs       = @()
+    [System.Collections.Generic.List[PSCustomObject]] $adContacts  = @()
+    [System.Collections.Generic.List[PSCustomObject]] $adComputers = @()
+    [System.Collections.Generic.List[PSCustomObject]] $adGroups    = @()
+    [System.Collections.Generic.List[PSCustomObject]] $adGPOs      = @()
+    [System.Collections.Generic.List[PSCustomObject]] $adUsers     = @()
+    # METHODS: Constructor method
     OZOMain($Configuration,$OutDir) {
         # Set properties
         $this.Logger   = (New-OZOLogger)
@@ -59,15 +63,45 @@ Class OZOMain {
         $this.Logger.Write("Starting process.","Information")
         # Determine if the configuation is valid
         If (($this.ValidateConfiguration() -And $this.ValidateEnvironment()) -eq $true) {
-            # Call GetItems to generate OZOItem objects from the JSON definition and set Success
-            $this.Success = $this.GetItems()
+            # Iterate through the JSON OU objects
+            ForEach ($adOU in $this.Json.ADOrganizationalUnits) {
+                # Add an OZOADOrganizationalUnit object to the adOUs list
+                $this.adOUs.Add(([OZOADOrganizationalUnit]::($adOU)))
+            }
+            # Iterate through the JSON Contacts objects
+            ForEach ($adContact in $this.Json.ADContacts) {
+                # Add an OZOADOContact object to the adContacts list
+                $this.adContacts.Add(([OZOADContact]::new($adContact)))
+            }
+            # Iterate through the JSON Computer objects
+            ForEach ($adComputer in $this.Json.ADComputers) {
+                # Add an OZOADComputer object to the adComputers list
+                $this.adComputers.Add(([OZOADComputer]::new($adComputer)))
+            }
+            # Iterate through the JSON Group objects
+            ForEach ($adGroup in $this.Json.ADGroups) {
+                # Add an OZOADGroup object to the adGroups list
+                $this.adGroups.Add(([OZOADGroup]::new($adGroup)))
+            }
+            # Iterate through the JSON GPO objects
+            ForEach ($adGPO in $this.Json.ADGroupPolicies) {
+                # Add an OZOADGroupPolicyObject object to the adOUs list
+                $this.adGPOs.Add(([OZOADGroupPolicyObject]::new($adOU)))
+            }
+            # Iterate through the JSON User objects
+            ForEach($adUser in $this.Json.ADUsers) {
+                # Add an OZOADUser object to the adUsers list
+                $this.adUsers.Add(([OZOADUser]::new($adUser,($this.adDomain).Name)))
+            }
+            # Manage default objects
+            $this.ManageDefaultObjects()
         }
         # Report
         $this.Report()
         # Log a process end message
         $this.Logger.Write(("Process complete."),"Information")
     }
-    # JSON validation method
+    # METHODS: JSON validation method
     Hidden [Boolean] ValidateConfiguration() {
         # Control variable
         [Boolean] $Return = $true
@@ -90,63 +124,265 @@ Class OZOMain {
         # Return
         return $Return
     }
-    # Environment validation method
+    # METHODS: Environment validation method
     Hidden [Boolean] ValidateEnvironment() {
         # Control variable
         [Boolean] $Return = $true
         # Determine if the outDir exists
         If ([Boolean](Test-Path -Path $this.outDir) -eq $true) {
             # Output directory exists; set the Excel path
-            $this.excelPath = (Join-Path -Path $this.outDir -ChildPath ((Get-OZO8601Date -Time) + "-" + $this.Json.ExcelFileName))
+            $this.excelPath = (Join-Path -Path $this.outDir -ChildPath ((Get-OZO8601Date -Time) + "-ozo-ad-manage-directory-objects.xlsx"))
         } Else {
             # Output directory does not exist; report
             $this.Logger.Write(("Output directory is invalid or inaccessible."),"Error")
             $Return = $false
         }
-        # Return
-        return $Return
-    }
-    # GetItems method
-    Hidden [Boolean] GetItems() {
-        # Control variable
-        [Boolean] $Return = $true
-        # Iterate through the items in the JSON
-        ForEach ($Item in $this.Json.Items) {
-            # Create an instance of the OZOItem class (an object)
-            $this.Items.Add(([OZOItem]::new($Item)))
-        }
-        # Determine that if the Item object count is not equal to the configuration item count
-        If ($this.Items.Count -ne $this.Json.Items.Count) {
-            # Item object count does not match configuration item count
-            $this.Success = $false
+        # Try to get the domain information
+        Try {
+            $this.adDomain = Get-ADDomain -ErrorAction Stop
+            # Success
+        } Catch {
+            # Failure
+            $this.Logger.Write("Failed to get AD domain information.")
+            $Return = $false
         }
         # Return
         return $Return
     }
-    # Report method
+    # METHODS: Manage default objects method
+    Hidden [Void] ManageDefaultObjects() {
+        # Iterate over the objects in the Users container
+        ForEach ($adObject in (Get-ADObject -Filter * -SearchBase $this.adDomain.UsersContainer)) {
+            # Switch on object class
+            Switch($adObject.ObjectClass) {
+                "user" {
+                    # Object class is user; move object to default users OU
+                    Move-ADObject -Identity $adObject.DistinguishedName -TargetPath $this.Json.ADDefaultObjectsOUs.Users
+                }
+                "group" {
+                    # Object class is group; move object to default groups OU
+                    Move-ADObject -Identity $adObject.DistinguishedName -TargetPath $this.Json.ADDefaultObjectsOUs.Groups
+                }
+            }
+        }
+    }
+    # METHODS: Report method
     Hidden [Void] Report() {
-        # Determine if any Items were created
-        If ($this.Items.Count -gt 0) {
-            # At least one Item was processed; output selected object properties to Excel
-            $this.Items | Select-Object -Property itemName,Validates,@{Name="Messages";Expression={$_.Messages -Join "; "}} | Export-Excel -WorksheetName $this.Json.ExcelWorksheetName -Path $this.excelPath
-            $this.Logger.Write(("See " + $this.excelPath + " for results."),"Information")
+        
+    }
+}
+
+Class OZOADContact {
+    # PROPERTIES: Booleans
+    [Boolean] $Success = $true
+    # PROPERTIES: String lists
+    [System.Collections.Generic.List[String]] $Messages = @()
+    # METHODS: Constructor method
+    OZOADContact($adContact) {
+        # Define parameters
+        $Parameters = @{
+            Name = ($adContact.FirstName + " " + $adContact.LastName)
+            Type = "contact"
+            Path = $adContact.Path
+            DisplayName = ($adContact.FirstName + " " + $adContact.LastName)
+            Mail = $adContact.EmailAddress
+            TelephoneNumber = $adContact.TelephoneNumber
+        }
+        # Determine if the contact does not already exist
+        If ([Boolean](Get-ADObject -Filter { Name -eq $adContact.Name -And ObjectClass -eq "contact" } -ErrorAction SilentlyContinue) -eq $false) {
+            # Contact does not already exist; try to create it
+            Try {
+                New-ADObject @Parameters -ErrorAction Stop
+                # Success
+            } Catch {
+                # Failure
+                $this.Messages.Add(("Contact creation failed with " + $_))
+                $this.Success = $false
+            }
+        } Else {
+            # Contact exists; skip
+            $this.Messages.Add("Contact already exists; skipping")
         }
     }
 }
 
-Class OZOItem {
-    # PROPERTIES: Booleans, Strings
-    [Boolean] $Validates = $true
-    [String]  $itemName  = $null
-    # PROPERTIES: Lists
+Class OZOADComputer {
+    # PROPERTIES: Booleans
+    [Boolean] $Success = $true
+    # PROPERTIES: String lists
     [System.Collections.Generic.List[String]] $Messages = @()
-    # METHODS
-    # Constructor method
-    OZOItem($Item) {
-        # Set properties
-        $this.itemName = $Item
-        # Log
-        $this.Messages.Add("Created new object of the OZOItem class called " + $Item)
+    # METHODS: Constructor method
+    OZOADComputer($adComputer) {
+        # Define parameters
+        $Parameters = @{
+            DisplayName = $adComputer.ComputerName
+            Enabled = $true
+            Name = $adComputer.ComputerName
+            Path = $adComputer.Path
+        }
+        # Determine if the computer does not already exist
+        If ([Boolean](Get-ADComputer -Identity $adComputer.Name) -eq $false) {
+            # Computer object does not already exist; try to create it
+            Try {
+                New-ADComputer @Parameters -ErrorAction Stop
+                # Success
+            } Catch {
+                # Failure
+                $this.Messages.Add(("Computer creation failed with " + $_))
+                $this.Success = $false
+            }
+        } Else {
+            # Computer object already exists; skip
+            $this.Messages.Add("Computer already exists; skipping")
+        }
+    }
+}
+
+Class OZOADGroup {
+    # PROPERTIES: Booleans
+    [Boolean] $Success = $true
+    # PROPERTIES: String lists
+    [System.Collections.Generic.List[String]] $Messages = @()
+    # METHODS: Constructor method
+    OZOADGroup($adGroup) {
+        # Define parameters
+        $Parameters = @{}
+        # Determine if the group does not already exist
+        If ([Boolean](Get-ADGroup -Identity $adGroup.Name -ErrorAction SilentlyContinue) -eq $false) {
+            # Group does not already exist; try to create it
+            Try {
+                New-ADGroup @Parameters -ErrorAction Stop
+                # Success; iterate over parent groups
+                ForEach ($Group in $adGroup.Groups) {
+                    # Try to add group to parent group
+                    Try {
+                        Add-ADGroupMember -Identity $Group -Members $adGroup.Name -ErrorAction Stop
+                        # Success
+                    } Catch {
+                        # Failure
+                        $this.Messages.Add(("Adding to parent group " + $Group + " failed with " + $_))
+                        $this.Success = $false
+                    }
+                }
+            } Catch {
+                # Failure
+                $this.Messages.Add(("Group creation failed with " + $_))
+                $this.Success = $false
+            }
+        } Else {
+            # Group already exists; skip
+            $this.Messages.Add("Group already exists; skipping")
+        }
+    }
+}
+
+Class OZOADGroupPolicyObject {
+    # PROPERTIES: Booleans
+    [Boolean] $Success = $true
+    # PROPERTIES: String lists
+    [System.Collections.Generic.List[String]] $Messages = @()
+    # METHODS: Constructor method
+    OZOADGroupPolicyObject($adGPO) {
+        # Determine if the GPO does not already exist
+        If ([Boolean](Get-GPO -Name $adGPO.Name) -eq $false) {
+            # GPO does not already exists; try to create it
+            Try {
+                New-GPO -Name $adGPO.Name -ErrorAction Stop
+                # Success; iterate over the Links
+                ForEach ($OU in $adGPO.Links) {
+                    # Try to link GPO to OU
+                    Try {
+                        New-GPLink -Name $adGPO.Name -Target $OU -ErrorAction Stop
+                        # Success
+                    } Catch {
+                        # Failure
+                        $this.Messages.Add(("Linking GPO to " + $OU + " failed with " + $_))
+                    }
+                }
+            } Catch {
+                # Failure
+                $this.Messages.Add(("Group Policy Object creation failed with " + $_))
+                $this.Success = $false
+            }
+        } Else {
+            # GPO does not already exist; skip
+            $this.Messages.Add("Group Policy Object already exists; skipping")
+        }
+    }
+}
+
+Class OZOADOrganizationalUnit {
+    # PROPERTIES: Booleans
+    [Boolean] $Success = $true
+    # PROPERTIES: String lists
+    [System.Collections.Generic.List[String]] $Messages = @()
+    # METHODS: Constructor method
+    OZOADOrganizationalUnit($adOU) {
+        # Generate DN
+        [String] $adOUDN = ("OU=" + $adOU.Name + "," + $adOU.Path)
+        # Define parameters
+        $Parameters = @{
+            Name = $adOU.Name
+            Path = $adOU.Path
+        }
+        # Determine if the OU does not already exist
+        If ([Boolean](Get-ADOrganizationalUnit -Identity $adOUDN) -eq $false) {
+            # OU does not already exist; try to create it
+            Try {
+                New-ADOrganizationalUnit @Parameters -ErrorAction Stop
+                # Success
+            } Catch {
+                # Failure
+                $this.Messages.Add(("Organizational Unit creation failed with " + $_))
+                $this.Success = $false
+            }
+        } Else {
+            # OU does not already exist; skip
+            $this.Messages.Add("Organizational Unit already exists; skipping")
+        }
+    }
+}
+
+Class OZOADUser {
+    # PROPERTIES: Booleans
+    [Boolean] $Success = $true
+    # PROPERTIES: String lists
+    [System.Collections.Generic.List[String]] $Messages = @()
+    # METHODS: Constructor method
+    OZOADUser($adUser,$adDomain) {
+        # Define parameters
+        $Parameters = @{
+            Name = ($adUser.GivenName + " " + $adUser.Surname)
+            SamAccountName = $adUser.SamAccountName
+            UserPrincipalName = ($adUser.SamAccountName + "@" + $adDomain)
+            AccountPassword = (ConvertTo-SecureString -AsPlainText -String $adUser.Password -Force)
+            Enabled = $true
+            Path = $adUser.Path
+        }
+        # Determine that the user does not already exist
+        If ([Boolean](Get-ADUser -Identity $adUser.SamAccountName -ErrorAction SilentlyContinue) -eq $false) {
+            # User does not already exist; try to create the user
+            Try {
+                New-ADUser @Parameters -ErrorAction Stop
+                # Success; iterate through the groups
+                ForEach ($adGroup in $adUser.Groups) {
+                    # Try to create the group
+                    Try {
+                        Add-AdGroupMember -Identity $adGroup -Members $adUser.SamAccountName -ErrorAction Stop
+                    } Catch {
+                        # Failure
+                        $this.Messages.Add(("Adding user to the " + $adGroup + " group failed with " + $_))
+                    }
+                }
+            } Catch {
+                # Failure
+                $this.Messages.Add(("User creation failed with " + $_))
+                $this.Success = $false
+            }
+        } Else {
+            # User already exists; skip
+            $this.Messages.Add("User already exists; skipping")
+        }
     }
 }
 
